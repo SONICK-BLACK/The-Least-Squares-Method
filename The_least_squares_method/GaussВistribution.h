27a55c70d9e5d6d03e56d8bbc328de01dd8ccdaf
SONICK-BLACK <mihasio150@gmail.com>
2024-04-09 20:59:06 +0300

    /

--- The_least_squares_method/GaussВistribution.h
+++ The_least_squares_method/GaussВistribution.h
@@ -5,7 +5,7 @@ using namespace std;
 class GaussÂistribution
 {
 public:
-	//Âñå õîğîøî
+	//Âñå õîğîøî/
 	double vectorRandY[100]; 
 	double vectorRandX[100];
 	GaussÂistribution(double mean, double sttdv, mt19937 h){
